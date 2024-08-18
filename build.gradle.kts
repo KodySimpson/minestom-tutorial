9a6796a22997299dc7268cbb28e26f4e7f3800e8
Kody Simpson <kodysimpson0616@gmail.com>
2024-08-18 14:15:36 -0500

    ready to go

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "me.kodysimpson"
@@ -16,6 +17,24 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.14")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Minestom has a minimum Java version of 21
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "me.kodysimpson.Main" // Change this to your main class
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("") // Prevent the -all suffix on the shadowjar file.
+    }
 }
